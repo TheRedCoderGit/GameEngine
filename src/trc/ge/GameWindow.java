cb35016747d47d46ef9d088793369c3391d647f7
Shai <Shai@DESKTOP-R593588>
2016-04-16 14:28:37 +0300

    Added GameWindow class

---
+++ src/trc/ge/GameWindow.java
@@ -0,0 +1,12 @@
+package trc.ge;
+
+import javax.swing.JPanel;
+
+public class GameWindow extends JPanel {
+
+	private static final long serialVersionUID = 7134616148923102510L;
+
+	public GameWindow(String title) {
+		
+	}
+}
